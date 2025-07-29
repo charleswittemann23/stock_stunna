31c6511e2854e5e9280754fd04960624397e2723
charleswittemann23 <charliewittemann@icloud.com>
2025-07-29 10:51:26 -0500

    update envs for parallel naming in prod and dev

--- backend/stock_portfolio/settings.py
+++ backend/stock_portfolio/settings.py
@@ -7,7 +7,7 @@ import dj_database_url
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Secret Key
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # Debug mode
 DEBUG = config('DEBUG', default=False, cast=bool)
